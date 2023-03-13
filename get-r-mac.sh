275e2bf3b99479749e829f3ae234b8d2d107e239
Jinhwan Kim <hwanistic@gmail.com>
2023-03-13 10:55:57 +0900

    initial commit

---
+++ get-r-mac.sh
@@ -0,0 +1,30 @@
+# Copyright (c) 2018 Dirk Schumacher, Noam Ross, Rich FitzJohn
+# Copyright (c) 2023 Jinhwan Kim
+
+#!/usr/bin/env bash
+set -e
+
+# Download and extract the main Mac Resources directory
+# Updated as big-sur / m1
+mkdir -p r-mac
+curl -o r-mac/latest_r.pkg \
+https://cloud.r-project.org/bin/macosx/big-sur-arm64/base/R-4.2.2-arm64.pkg
+
+cd r-mac
+xar -xf latest_r.pkg
+rm -r Resources tcltk.pkg texinfo.pkg Distribution latest_r.pkg
+# cat R-app.pkg/Payload | gunzip -dc | cpio -i
+cat R-fw.pkg/Payload | gunzip -dc | cpio -i
+mv R.framework/Versions/Current/Resources/* .
+rm -r R-fw.pkg R.framework
+
+# Patch the main R script
+sed -i.bak '/^R_HOME_DIR=/d' bin/R
+sed -i.bak 's;/Library/Frameworks/R.framework/Resources;${R_HOME};g' \
+bin/R
+chmod +x bin/R
+rm -f bin/R.bak
+
+# Remove unneccessary files
+rm -r doc tests
+rm -r lib/*.dSYM
